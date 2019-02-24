f5efcfebac948fb2e2ed07f6d78cf0d36b4193b7
Akash <akashnainani9@gmail.com>
2019-02-24 10:16:07 +0530

    heroku file upload test

--- app.py
+++ app.py
@@ -1,17 +1,33 @@
 from flask import Flask
+from flask import render_template, request
 from datetime import datetime
+import os
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
-
     return """
-    <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
+    <html>
+    <body>
+      <form action = "http://localhost:5000/uploader" method = "POST" 
+           enctype = "multipart/form-data">
+           <input type = "file" name = "file" />
+           <input type = "submit"/>
+           </form>
+         </body>
+    </html>
+    """
 
-    <img src="http://loremflickr.com/600/400" />
-    """.format(time=the_time)
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+      f = request.files['file']
+      f.save(f.filename)
+      if os.path.isfile(f.filename):
+         return 'file uploaded successfully'
+      else:
+          return 'file upload fail'    
+
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
