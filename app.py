3df07912e2f4afdcdc05b2c70913701165e47875
julioRAC <165182103+julioRAC@users.noreply.github.com>
2024-05-20 15:00:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,18 +4,18 @@ import os
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return "Bienvenido a la API de raíz cuadrada. Usa la ruta /sqrt con el parámetro number para obtener la raíz cuadrada de un número."
+
 @app.route('/sqrt', methods=['GET'])
 def get_sqrt():
     try:
-        # Obtiene el parámetro 'number' de la consulta
         number = request.args.get('number', type=float)
         if number is None:
             return jsonify({'error': 'No se proporcionó ningún número.'}), 400
 
-        # Calcula la raíz cuadrada
         sqrt_result = math.sqrt(number)
-
-        # Retorna el resultado en formato JSON
         return jsonify({'number': number, 'sqrt': sqrt_result})
     except ValueError:
         return jsonify({'error': 'Proporcione un número válido.'}), 400
