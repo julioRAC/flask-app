b22b144a5ad1a6a919b3d33ec7dd5e9116499aca
julioRAC <165182103+julioRAC@users.noreply.github.com>
2024-05-20 14:26:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
-from flask import Flask
-from datetime import datetime
+from flask import Flask, request, jsonify
+import math
+
 app = Flask(__name__)
 
-@app.route('/')
-def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+@app.route('/sqrt', methods=['GET'])
+def get_sqrt():
+    try:
+        # Obtiene el parámetro 'number' de la consulta
+        number = request.args.get('number', type=float)
+        if number is None:
+            return jsonify({'error': 'No se proporcionó ningún número.'}), 400
 
-    return """
-    <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
+        # Calcula la raíz cuadrada
+        sqrt_result = math.sqrt(number)
 
-    <img src="http://loremflickr.com/600/400" />
-    """.format(time=the_time)
+        # Retorna el resultado en formato JSON
+        return jsonify({'number': number, 'sqrt': sqrt_result})
+    except ValueError:
+        return jsonify({'error': 'Proporcione un número válido.'}), 400
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
-
+    app.run(debug=True)
